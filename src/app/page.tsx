0e196dc5b9dd622f2bc95c9a2b028b554ecc6b76
Mariam Mohammed <mariam.mohammed@and.digital>
2024-07-08 14:47:11 +0100

    quizfirsttask

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>Your code here</main>;
-}
+
+   const data =  
+    {
+        questions: [
+          {
+            id: 'abc123457u9',
+            question: 'What is the capital of France?',
+            answers: [
+              { text: 'London', isCorrect: false },
+              { text: 'Paris', isCorrect: true },
+              { text: 'New York', isCorrect: false },
+              { text: 'Dublin', isCorrect: false },
+            ],
+          },
+        ],
+      };
+
+  return (
+   <main className={styles.main}>
+    <h1>Question 1/x</h1>
+    {data.questions.map(question => (
+        <>
+        <div>{question.question}</div>
+        <div>{}</div>
+        </>))}
+  </main>
+    );
+};
